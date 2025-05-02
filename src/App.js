dcdcc380332988963fe3b1a7ea06a6fcf8210726
gokulakrishnan <gokulhari182@gmail.com>
2025-05-02 12:51:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/Offer"; // Home page component
