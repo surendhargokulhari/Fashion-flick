ff81013cc4e301979f0ef44a4599b9a9a347939e
gokulakrishnan <gokulhari182@gmail.com>
2025-06-15 14:12:08 +0530

    commit

--- src/components/PaymentPage.js
+++ src/components/PaymentPage.js
@@ -57,7 +57,7 @@ const PaymentPage = () => {
   };
 
   try {
-    const response = await fetch("http://localhost:5000/api/orders", {
+    const response = await fetch("https://fashion-backend-yvih.onrender.com/api/orders", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newOrder),
